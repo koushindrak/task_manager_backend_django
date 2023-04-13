bf4d9a7356ce4e7eba23dc7e108ed3d859f0fff0
saumyak05 <128255046+saumyak05@users.noreply.github.com>
2023-04-14 00:08:05 +0530

    Update settings.py

--- task_manager_3_dj/settings.py
+++ task_manager_3_dj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-1cp#_=tejrddakuy6b)*zi^bxx6t&(=kz%j_)uos_u*+^$%y)y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 #NEW
 # REST_FRAMEWORK = {
 #     'DEFAULT_PERMISSION_CLASSES': [
